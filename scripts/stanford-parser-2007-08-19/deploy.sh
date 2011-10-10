75ad40cabccc1543b36b7c83f59fab4db4a7470c
Renaud Richardet <renaud@apache.org>
2011-10-10 17:04:15 +0200

    another standford

--- scripts/stanford-parser-2007-08-19/deploy.sh
+++ scripts/stanford-parser-2007-08-19/deploy.sh
@@ -9,7 +9,7 @@ REPO=snapshot-repo
 URL=file:/Users/ren/dev/sources/maven_repo/snapshots/
 REMOTE_URL=https://github.com/renaud/maven_repo/raw/master/snapshots
 
-mvn source:jar deploy:deploy-file -Dfile=$FILE  -DgroupId=$GROUP_ID -DartifactId=$ARTIFACT_ID -Dversion=$VERSION -Dpackaging=jar -DrepositoryId=$REPO -Durl=$URL
+mvn deploy:deploy-file -Dfile=$FILE  -DgroupId=$GROUP_ID -DartifactId=$ARTIFACT_ID -Dversion=$VERSION -Dpackaging=jar -DrepositoryId=$REPO -Durl=$URL
 
 echo '\n\n<dependency>\n<groupId>'$GROUP_ID'</groupId>\n<artifactId>'$ARTIFACT_ID'</artifactId>\n<version>'$VERSION'</version>\n</dependency>\n\n'
 echo '\n<repositories>\n<repository>\n<id>'$REPO'</id>\n<url>'$REMOTE_URL'</url>\n</repository>\n</repositories>\n\n'
