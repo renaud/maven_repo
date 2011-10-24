ecdfd0f1454415a38c5fd3047c705f7214079b0d
Renaud Richardet <renaud@apache.org>
2011-10-24 11:22:58 +0200

    added trove, mallet-troveless

---
+++ scripts/mallet-troveless-0.4/deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+FILE=mallet-troveless-0.4.jar
+GROUP_ID=mallet
+ARTIFACT_ID=mallet-troveless
+VERSION=0.4
+
+REPO=snapshot-repo
+URL=file:/Users/ren/dev/sources/maven_repo/snapshots/
+REMOTE_URL=https://github.com/renaud/maven_repo/raw/master/snapshots
+
+mvn deploy:deploy-file -Dfile=$FILE  -DgroupId=$GROUP_ID -DartifactId=$ARTIFACT_ID -Dversion=$VERSION -Dpackaging=jar -DrepositoryId=$REPO -Durl=$URL
+
+echo '\n\n<dependency>\n<groupId>'$GROUP_ID'</groupId>\n<artifactId>'$ARTIFACT_ID'</artifactId>\n<version>'$VERSION'</version>\n</dependency>\n\n'
+echo '\n<repositories>\n<repository>\n<id>'$REPO'</id>\n<url>'$REMOTE_URL'</url>\n</repository>\n</repositories>\n\n'
+
+echo 'cd to '$URL
