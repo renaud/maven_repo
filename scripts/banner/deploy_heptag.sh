b4d5e7d4c387300d953c2521d18c4a4932d4a53b
Renaud Richardet <richarde@tsf-wpa-3-1195.epfl.ch>
2011-12-01 11:23:11 +0100

    medpost, heptag

---
+++ scripts/banner/deploy_heptag.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+FILE=heptag.jar
+GROUP_ID=banner
+ARTIFACT_ID=heptag
+VERSION=20111108-SNAPSHOT
+
+REPO=snapshot-repo
+URL=file:/Users/richarde/dev/sources/maven_repo/snapshots/
+REMOTE_URL=https://github.com/renaud/maven_repo/raw/master/snapshots
+
+mvn deploy:deploy-file -Dfile=$FILE  -DgroupId=$GROUP_ID -DartifactId=$ARTIFACT_ID -Dversion=$VERSION -Dpackaging=jar -DrepositoryId=$REPO -Durl=$URL
+
+echo '\n\n<dependency>\n<groupId>'$GROUP_ID'</groupId>\n<artifactId>'$ARTIFACT_ID'</artifactId>\n<version>'$VERSION'</version>\n</dependency>\n\n'
+echo '\n<repositories>\n<repository>\n<id>'$REPO'</id>\n<url>'$REMOTE_URL'</url>\n</repository>\n</repositories>\n\n'
+
+echo 'cd to '$URL
